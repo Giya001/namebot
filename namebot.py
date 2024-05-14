1a97b479ef789c76563140432c13b941732f47b8
Giya001 <giyoshikmatillayev@gmail.com>
2024-05-14 20:24:01 +0500

    Update namebot.py
    Version 2

--- namebot.py
+++ namebot.py
@@ -16,3 +16,23 @@ def start(message):
                types.InlineKeyboardButton('USD', callback_data='USD'),
                types.InlineKeyboardButton('RUB', callback_data='RUB'), )
     bot.send_message(message.chat.id, 'Choose your currency', reply_markup=markup)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    querystring = {'from': call.data, 'to': 'UZS', 'amount': '1'}
+
+    headers = {
+        'X-RapidAPI-Key': '',
+        'X-RapidAPI-Host': 'currency-converter-pro1.p.rapidapi.com',
+    }
+
+    response = requests.get(API, headers=headers, params=querystring)
+
+    if response.status_code == 200:
+        data = response.json()
+        conversion_result = data['result']
+        bot.send_message(call.message.chat.id, f'Conversion result :{conversion_result}')
+    else:
+        bot.send_message(call.message.chat.id, f'Failed to perform conversion. Please try again later.')
+
+
+bot.polling()
